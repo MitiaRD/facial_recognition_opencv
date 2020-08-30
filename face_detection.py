7fa9370ba7705f9cc3c79926287ae110ebd647bc
Mitia <66083535+MitiaRD@users.noreply.github.com>
2020-08-30 22:37:17 +0100

    Update face_detection.py
    added comments

--- face_detection.py
+++ face_detection.py
@@ -2,16 +2,23 @@ import cv2
 import sys
 import os
 
+#defining paths for the images and haar cascade
 path = 'known_faces'
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
+#change the directory to loop through the images
 os.chdir(path)
 
+#looping through images in the current directory
 for images in os.listdir():
+    #avoiding filenot found error
     if images.endswith('.jpg'):
+        #reading the image
         image = cv2.imread(images)
+        #converting image to gray to reduce information in image
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #using the haar cascade to find faces
         faces = faceCascade.detectMultiScale(
             gray,
             scaleFactor=1.1,
@@ -19,6 +26,7 @@ for images in os.listdir():
             minSize=(10, 10)
             #flags = cv2.CV_HAAR_SCALE_IMAGE
         )
+        #drawing boxes around the faces found
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
         cv2.imshow("Faces found", image)
